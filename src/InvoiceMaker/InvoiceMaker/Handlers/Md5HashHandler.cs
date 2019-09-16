7eedbc4afd2860f5f8f43b1c00f9ee6dd77b38e2
James Churchill <james@smashdev.com>
2019-09-16 07:17:15 -0700

    Stub out the Md5 hash handler

---
+++ src/InvoiceMaker/InvoiceMaker/Handlers/Md5HashHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InvoiceMaker.Handlers
+{
+    public class Md5HashHandler : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Write("HEY!");
+        }
+    }
+}
